a9e0e43b91d39bcb986337d6fa20004e523d4883
Dave Anderson <dave@pentuple.co.nz>
2018-09-06 14:56:43 +1200

    [29272] Streamline lot/serial traceability on Transfer Orders. Plue resolve a bunch of bugs

--- widgets/xcombobox.cpp
+++ widgets/xcombobox.cpp
@@ -1011,7 +1011,6 @@ void XComboBox::setListIdFieldName(QString p)
   _data->_listIdFieldName = p;
 }
 
-// exists only for script exposure
 void XComboBox::removeItem(int idx)
 {
   QComboBox::removeItem(idx);
