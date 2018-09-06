1500a61494e1d00c50f124ccdb68b261a7980286
Michael Micros <mmicros@xtuple.com>
2018-09-06 14:30:08 -0400

    added xTuple copyright

--- widgets/exportOptions.h
+++ widgets/exportOptions.h
@@ -1,10 +1,12 @@
-/********************************************************************************
-** Form generated from reading UI file 'exportOptions.ui'
-**
-** Created by: Qt User Interface Compiler version 5.5.1
-**
-** WARNING! All changes made in this file will be lost when recompiling UI file!
-********************************************************************************/
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2018 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
 
 #ifndef EXPORTOPTIONS_H
 #define EXPORTOPTIONS_H
