7a3c13231c1d4d4c6289fa752a2f9decc45e0a71
Dave Anderson <dave@pentuple.co.nz>
2019-03-14 09:18:40 +1300

    Move employee ancilliary deletions to triggers

--- guiclient/employees.cpp
+++ guiclient/employees.cpp
@@ -80,45 +80,21 @@ void employees::sView()
 
 void employees::sDelete()
 {
+  XSqlQuery delq;
+
   if (QMessageBox::question(this, tr("Delete?"),
                             tr("Are you sure you want to delete this Employee?"),
                             QMessageBox::Yes,
                             QMessageBox::No | QMessageBox::Default) == QMessageBox::No)
     return;
 
-  XSqlQuery rollbackq;
-  rollbackq.prepare("ROLLBACK;");
-  XSqlQuery begin("BEGIN;");
-
-  XSqlQuery delq;
-  delq.prepare("DELETE FROM charass WHERE charass_target_type = 'EMP' AND charass_target_id = :emp_id;");
-  delq.bindValue(":emp_id", list()->id());
-  delq.exec();
-  if (ErrorReporter::error(QtCriticalMsg, this, tr("Error Deleting Employee"),
-                           delq, __FILE__, __LINE__))
-  {
-    rollbackq.exec();
-    return;
-  }
-  delq.prepare("DELETE FROM empgrpitem WHERE groupsitem_reference_id = :emp_id;");
-  delq.exec();
-  if (ErrorReporter::error(QtCriticalMsg, this, tr("Error Deleting Employee"),
-                           delq, __FILE__, __LINE__))
-  {
-    rollbackq.exec();
-    return;
-  }
   delq.prepare("DELETE FROM emp WHERE emp_id = :emp_id;");
   delq.bindValue(":emp_id", list()->id());
   delq.exec();
   if (ErrorReporter::error(QtCriticalMsg, this, tr("Error Deleting Employee"),
                            delq, __FILE__, __LINE__))
-  {
-    rollbackq.exec();
     return;
-  }
 
-  delq.exec("COMMIT;");
   sFillList();
 }
 
